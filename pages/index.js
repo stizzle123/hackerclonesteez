604264b3426542b1a61f6bb17903d4d34449e9bc
Idowu <iarifayan@russelsmith.local>
2019-11-06 10:11:24 +0100

    Modified next.config file

--- pages/index.js
+++ pages/index.js
@@ -26,16 +26,14 @@ class Index extends React.Component {
 
   componentDidMount() {
     if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function() {
-        navigator.serviceWorker
-          .register("/service-worker.js", { scope: "/" })
-          .then(function(registration) {
-            console.log("SW registered: ", registration);
-          })
-          .catch(function(registrationError) {
-            console.log("SW registration failed: ", registrationError);
-          });
-      });
+      navigator.serviceWorker
+        .register("/service-worker.js", { scope: "/" })
+        .then(function(registration) {
+          console.log("SW registered: ", registration);
+        })
+        .catch(function(registrationError) {
+          console.log("SW registration failed: ", registrationError);
+        });
     }
     // register();
   }
