44a177457f08f598807db4d3df0766442c00ff63
Idowu <iarifayan@russelsmith.local>
2019-11-06 12:26:11 +0100

    Modified public

---
+++ public/precache.TO3Wro7FI1C242RjKKFVn.96815d5225a14ad0f74e4e80cdb86994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([ { "url": "/", "revision": "TO3Wro7FI1C242RjKKFVn" },
+  {
+    "revision": "073c94d061c2fac9c12d",
+    "url": "/_next/static/chunks/commons.f8c784af705d9033fcb0.js"
+  },
+  {
+    "revision": "9f098f266d9231709580",
+    "url": "/_next/static/runtime/main-f524de4ed444894a3e84.js"
+  },
+  {
+    "revision": "be4b6cc6d10632d2262c",
+    "url": "/_next/static/runtime/webpack-035ac2b14bde147cb4a8.js"
+  },
+  {
+    "revision": "45d27692b16a10153f8b",
+    "url": "/_next/static/TO3Wro7FI1C242RjKKFVn/pages/_app.js"
+  },
+  {
+    "revision": "981ec3cdd0813fd556cb",
+    "url": "/_next/static/TO3Wro7FI1C242RjKKFVn/pages/_error.js"
+  },
+  {
+    "revision": "7c856083ce966c306a08",
+    "url": "/_next/static/TO3Wro7FI1C242RjKKFVn/pages/index.js"
+  },
+  {
+    "revision": "dc8accac733c2fc5d803",
+    "url": "/_next/static/TO3Wro7FI1C242RjKKFVn/pages/story.js"
+  }
+]);
